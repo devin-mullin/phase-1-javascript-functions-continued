5f6af4a8b268621254ba301c0bdc94dd6470da82
Devin Mullin <devin.mullin90@gmail.com>
2021-08-31 17:47:36 -0500

    commit tree 3ef3141384e37682226a1a7dc5114c1ba0959591

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+saturdayFun()
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork()
+mondayWork("work from home")
+
+function wrapAdjective(mark = "*"){
+        return function(adj = "a good person") {
+            return `You are ${mark}${adj}${mark}!`
+        }
+}
+
+wrapAdjective()
+wrapAdjective("|")("a dedicated programmer")
+
+
+
+
+
